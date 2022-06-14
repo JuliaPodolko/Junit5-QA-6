ce7b2af5d910694eee6f3f76cc9a734676217492
Julia Podolko <Julia Podolkoemail>
2022-06-14 20:01:05 +0500

    added @MethodSource and @ValueSource to HomeWork

--- src/test/java/guru/qa/HomeWork.java
+++ src/test/java/guru/qa/HomeWork.java
@@ -1,12 +1,20 @@
 package guru.qa;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class HomeWork {
@@ -18,6 +26,10 @@ public class HomeWork {
     @BeforeEach
     void setMod () {
         open ("/text-box");
+    }
+    @AfterEach
+    void close(){
+        Selenide.closeWebDriver();
     }
     @CsvSource (value = {
             "Anna Petrova | anna@test.ru",
@@ -45,8 +57,34 @@ public class HomeWork {
                 text(fullName),
                 text(userEmail));
     }
-    @AfterEach
-    void close(){
-        Selenide.closeWebDriver();
+    static Stream<Arguments> methodSourceExampleTest() {
+        return  Stream.of(
+                Arguments.of("Anna Petrova", "anna@test.ru"),
+                Arguments.of("Maria Ivanova", "maria@test.ru")
+        );
+    }
+    @MethodSource("methodSourceExampleTest")
+    @ParameterizedTest
+    void methodSourceExampleTest(String fullName, String userEmail) {
+        $("#userName").setValue(fullName); // Name
+        $("#userEmail").setValue(userEmail); // Электронная почта
+        $("#submit").click();
+        $("#output").shouldHave(
+                text(fullName),
+                text(userEmail));
+    }
+    @ValueSource(strings = {
+            "Anna Petrova",
+            "Maria Ivanova"
+    })
+    @ParameterizedTest(name="Проверка заполнения формы {0}")
+    void fillFormTest (String fullName) {
+        String userEmail = "test@test.ru";
+        $("#userName").setValue(fullName); // Name
+        $("#userEmail").setValue(userEmail); // Электронная почта
+        $("#submit").click();
+        $("#output").shouldHave(
+                text(fullName),
+                text(userEmail));
     }
 }
